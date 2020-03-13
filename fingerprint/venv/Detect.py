43affd1d01dfd31544401764b89580d3cc2ee3b8
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2020-03-13 15:58:19 +0100

    Dodanie funkcji do zaokrąglania śladu palca po segmentacji.

--- fingerprint/venv/Detect.py
+++ fingerprint/venv/Detect.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import sys
 
-def LoadImagesFromFolder(folder):
+def LoadImagesFromFolder(folder): #załadowanie obrazów z wskazanego folderu
     images = []
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder,filename))
@@ -10,7 +10,7 @@ def LoadImagesFromFolder(folder):
             images.append(img)
     return images
 
-def FingerprintSegmentation(image, theshold):
+def FingerprintSegmentation(image, theshold): #rozdzelenie obrazu tak, żeby wyciąć tło z prawej oraz elwej strony
     left = []
     right = []
     for y in range(image.shape[0]):
@@ -18,16 +18,67 @@ def FingerprintSegmentation(image, theshold):
             if(image[y, x] < theshold):
                 left.append(x)
                 break;
+            if(x == image.shape[1] - 1):
+                left.append(image.shape[1] - 1)
     for y in range(image.shape[0]):
         for x in range(image.shape[1]):
             if (image[y, image.shape[1] - x - 1] < theshold):
                 right.append(image.shape[1] - x - 1)
                 break;
+            if (x == image.shape[1] - 1):
+                right.append(0)
 
     return left, right
 
+def MedianBlur1D(array, power):
+    if(len(array) == 0):
+        print("This array no have elements")
+        return array
+    newArray = []
+    for p in range(power): #dodanie początku bez zmian
+        newArray.append(array[p])
 
-cropSize = [sys.maxsize, sys.maxsize, 0, 0] #rozmiar przycięcia obrazu
+    for i in range(power, len(array) - power): #użycie filtru medianowego do rozycia obrazu
+        medians = []
+        for p in range(power * 2 + 1):
+            medians.append(array[i - power + p])
+        medians.sort()
+        newArray.append(medians[int(len(medians) / 2)])
+
+    for p in range(power): #dodanie końca bez zmian
+        newArray.append(array[len(array) - power + p])
+
+    return newArray
+
+def GetStartPoint():
+    startPoint = (10, 10)
+    # do
+    # {
+    #     while( aktualny węzeł należy do tła ∨ aktualny węzeł był odwiedzony )
+    #     {
+    #         przejdź do następnego węzła siatki;
+    #     }
+    #     punkt początkowy = aktualny węzeł siatki;
+    #     wyznacz kierunek linii w nowym punkcie początkowym;
+    #     utwórz sekcję w oparciu o wyznaczony startPoint i kierunek linii;
+    #     startPoint = minimum utworzonej sekcji;
+    # }
+    # while ( startPoint należy do tła ∨ punkt początkowy był odwiedzony );
+
+    return startPoint;
+
+def CheckStop():
+    stop = 0
+    # if ( nowe minimum leży poza rejonem wysegmentowanym z tła ):
+    #     kryterium stopu = opuszczenie obszaru odcisku
+    # elif( przeskoczono pomiędzy liniami ):
+    #     kryterium stopu = zakończenie linii
+    # elif( nowe minimum jest oznaczone jako już odwiedzone ):
+    #     kryterium stopu = przecięcie linii;
+
+    return stop;
+
+
 
 images = LoadImagesFromFolder('./../../../probki/DB1_B/')
 #images += LoadImagesFromFolder('./../../../probki/DB2_B/')
@@ -51,6 +102,7 @@ while True:
         if imageIndex < len(images) - 1:
             imageIndex += 1
     elif key == ord('w'):
+        cropSize = [sys.maxsize, sys.maxsize, 0, 0]  # rozmiar przycięcia obrazu
         orginal = images[imageIndex].copy()
         gray = cv2.cvtColor(orginal, cv2.COLOR_BGR2GRAY)
         #ograniczenie rozmiaru przetwarzanego obrazu do samego odcisku palca (wykasowanie tła)
@@ -69,9 +121,16 @@ while True:
         crop = gray[cropSize[1]:cropSize[3], cropSize[0]:cropSize[2]]
         #zastosowanie filtru medianowego w celu usunięcia z obrazu porów
         crop = cv2.medianBlur(crop, 3)
-        #segmentacjia obrazu palca tak aby uzystakć tylko obrys palca
+        #segmentacjia obrazu palca tak aby uzystakć tylko obrys palca + wygładzenie
         left, right = FingerprintSegmentation(crop, 230)
-        
-        cv2.imshow('orginal', crop)
+        left = MedianBlur1D(left, 4)
+        right = MedianBlur1D(right, 4)
+        #print(str(len(left)) + " " + str(len(right)) + " " + str(crop.shape[0]) + " " + str(crop.shape[1])) #DEBUG
+        for y in range(crop.shape[0]):
+            for x in range(crop.shape[1]):
+                if(x < left[y] or x > right[y]):
+                    crop[y, x] = 30
+
+        cv2.imshow('test', crop)
 
 cv2.destroyAllWindows()
